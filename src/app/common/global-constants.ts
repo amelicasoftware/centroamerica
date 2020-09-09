a69804f216894890beb0a057b827caad5f69ab85
Luis Angel Angeles <angel_apineda@hotmail.com>
2020-09-08 21:56:50 -0500

    prueba revistas pais

---
+++ src/app/common/global-constants.ts
@@ -0,0 +1,11 @@
+export class GlobalConstants {
+
+    // public static url: string = 'http://localhost:8080/';
+    // public static serviciosURL: string = 'http://localhost:8080/BackEndAmelic-0.0.1-SNAPSHOT/centroamerica/';
+    // public static url: string = 'http://localhost:4200/';
+    //produccion
+    public static serviciosURL: string = 'http://portal.amelica.org/BackEndAmelic/centroamerica/';
+    public static url: string = 'http://regions.amelica.org/centroamerica/';
+    // public static url: string = 'http://semantic.redalyc.org/conocimientoancestral/';
+    public static siteTitle: string = 'Centroamérica';
+}
